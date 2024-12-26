fe100bbfb4b2b3990f306a31cdec1443e7838f50
Alumist <codealumist@gmail.com>
2024-12-25 18:37:37 -0800

    hidden

--- app/components/features.tsx
+++ app/components/features.tsx
@@ -46,12 +46,12 @@ function Features() {
               data-target='panel-3'
             >
               <div className='py-5' data-target='panel-3'>
-                Diffecult Business Plan
+                Difficult Business Plan
               </div>
             </div>
           </div>
           {/* panels */}
-          <div id='panels' className='container mx-auto'>
+          <section id='panels' className='container mx-auto'>
             {/* first pan */}
             <div className='flex flex-col py-5 md:flex-row md:space-x-7 panel panel-1'>
               {/* img */}
@@ -83,7 +83,71 @@ function Features() {
                 </div>
               </div>
             </div>
-          </div>
+
+            {/* 2 pan */}
+            <div className='flex flex-col hidden py-5 md:flex-row md:space-x-7 panel panel-1'>
+              {/* img */}
+              <div className='flex justify-center md:w-1/2'>
+                <Image
+                  src='/images/annoyed.jpg'
+                  alt='Smiles'
+                  width={300}
+                  height={300}
+                  className='relative z-10 rounded-md'
+                />
+              </div>
+              {/* text */}
+              <div className='flex flex-col space-y-8 md:w-1/2'>
+                <h3 className='mt-32 text-3xl font-semibold text-center md:mt-0 md:text-left'>
+                  Not So Simple Plan
+                </h3>
+                <p className='max-w-md text-center text-gray-600 md:text-left'>
+                  This is a simple business plan that can be done in 15 minutes.
+                  As 'easy' as a drawn stick figure, with a very little detail.
+                </p>
+                <div className='mx-auto md:mx-0'>
+                  <Link
+                    href='#'
+                    className='px-2 py-3 mt-4 font-semibold text-white rounded-lg md:inline-flex bg-blue-600 hover:bg-blue-400 '
+                  >
+                    Learn More
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            {/* 3 pan */}
+            <div className='flex flex-col hidden py-5 md:flex-row md:space-x-7 panel panel-1'>
+              {/* img */}
+              <div className='flex justify-center md:w-1/2'>
+                <Image
+                  src='/images/angry.jpg'
+                  alt='Smiles'
+                  width={300}
+                  height={300}
+                  className='relative z-10 rounded-md'
+                />
+              </div>
+              {/* text */}
+              <div className='flex flex-col space-y-8 md:w-1/2'>
+                <h3 className='mt-32 text-3xl font-semibold text-center md:mt-0 md:text-left'>
+                  Difficult Plan
+                </h3>
+                <p className='max-w-md text-center text-gray-600 md:text-left'>
+                  This is a Difficult business plan that can be done in a 30
+                  minutes. As hard as a drawn stick figure, with details.
+                </p>
+                <div className='mx-auto md:mx-0'>
+                  <Link
+                    href='#'
+                    className='px-2 py-3 mt-4 font-semibold text-white rounded-lg md:inline-flex bg-blue-600 hover:bg-blue-400 '
+                  >
+                    Learn More
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
     </>
